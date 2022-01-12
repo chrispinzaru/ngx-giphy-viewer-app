295c47f31fcea12f871d72f33b63377acbbc1ddb
Chris Pinzaru <devpinzaru@gmail.com>
2022-01-12 14:14:46 +0200

    Install ng-bootstrap

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+    NgbPaginationModule,
+    NgbButtonsModule,
+} from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule, NgbPaginationModule, NgbButtonsModule],
+})
+export class SharedModule {}
