2e82e7f2f9e439603004520679e84e17c4e44790
Chris Pinzaru <devpinzaru@gmail.com>
2022-01-14 11:17:51 +0200

    Enhance gif pagination

--- src/app/shared/gif-pagination/gif-pagination.component.ts
+++ src/app/shared/gif-pagination/gif-pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
     selector: 'ngv-gif-pagination',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
     styleUrls: ['./gif-pagination.component.scss'],
 })
 export class GifPaginationComponent {
+    @Input() page: number;
+    @Input() count: number;
+    @Input() totalCount: number;
+
+    @Output() pageChange: EventEmitter<number> = new EventEmitter<number>();
+
     constructor() {}
+
+    onPageChange(page: number) {
+        this.pageChange.emit(page);
+    }
 }
