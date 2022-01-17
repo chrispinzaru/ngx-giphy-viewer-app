d967b07ffe20685c3876236bb50755afaa32e0fd
Chris Pinzaru <devpinzaru@gmail.com>
2022-01-17 09:53:49 +0200

    Refactor search-form, use more standalone testable methods/helpers

--- src/app/shared/search-form/search-form.component.ts
+++ src/app/shared/search-form/search-form.component.ts
@@ -7,14 +7,9 @@ import { FormControl, FormGroup } from '@angular/forms';
     styleUrls: ['./search-form.component.scss'],
 })
 export class SearchFormComponent {
-    @Input() set query(searchQuery: string) {
-        const rawSearchTags = this.parseQueryTags(searchQuery);
-        if (rawSearchTags) {
-            this.searchTags = [...new Set(rawSearchTags)];
-            this.emitSearchQueryChange();
-        }
+    @Input() set query(value: string) {
+        this.addSearchTags(this.parseSearchTags(value));
     }
-
     @Output() queryChange: EventEmitter<string> = new EventEmitter<string>();
 
     public searchForm = new FormGroup({
@@ -25,33 +20,46 @@ export class SearchFormComponent {
 
     constructor() {}
 
-    public emitSearchQueryChange() {
-        this.queryChange.emit(this.searchTags.join(' '));
-    }
-
-    public onSubmit() {
-        const query = this.searchForm.value.query;
-        const rawSearchTags = this.parseQueryTags(query).filter(
-            (tag) => !this.searchTags.includes(tag)
+    onClickSubmit() {
+        const newSearchTags = this.parseSearchTags(this.searchQuery).filter(
+            (tag) => !this.hasSearchTag(tag)
         );
-
-        if (rawSearchTags.length) {
-            this.searchTags = [...this.searchTags, ...rawSearchTags];
-            this.emitSearchQueryChange();
+        if (newSearchTags.length) {
+            this.addSearchTags(newSearchTags);
+            this.emitQueryChange();
         }
 
         this.searchForm.reset();
     }
 
-    public onTagRemove(tag: string) {
-        const tagIndex = this.searchTags.indexOf(tag);
-        if (tagIndex !== -1) {
-            this.searchTags.splice(tagIndex, 1);
-            this.emitSearchQueryChange();
+    onClickTagRemove(tag: string) {
+        if (this.removeSearchTag(tag)) {
+            this.emitQueryChange();
         }
     }
 
-    private parseQueryTags(searchQuery: string = ''): string[] {
+    private get searchQuery(): string {
+        return this.searchForm.value.query;
+    }
+
+    private emitQueryChange() {
+        this.queryChange.emit(this.searchTags.join(' '));
+    }
+
+    private parseSearchTags(searchQuery: string = ''): string[] {
         return searchQuery.split(' ').filter((tag: string) => tag.length > 0);
+    }
+
+    private addSearchTags(tags: string[] = []) {
+        this.searchTags = [...new Set([...this.searchTags, ...tags])];
+    }
+
+    private removeSearchTag(tag: string, tags = this.searchTags): boolean {
+        const tagIndex = tags.indexOf(tag);
+        return tagIndex === -1 ? false : !!tags.splice(tagIndex, 1).length;
+    }
+
+    private hasSearchTag(tag: string, tags = this.searchTags): boolean {
+        return tags.includes(tag);
     }
 }
